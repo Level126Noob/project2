2ed97b593eaf18fde9956b6eee86551b143752c1
Travis Abraham <abe.travis@outlook.com>
2019-08-17 09:20:49 -0700

    made basic file structure, using handlebars, made server.js to connect to localhost, schema file, and css made. linked public with static

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: ""
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
