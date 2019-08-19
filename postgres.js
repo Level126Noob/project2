287427e5b5a82d31b9f3ffacfebdce3669c4ca73
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-08-19 18:35:08 -0400

    CONNECTED TO POSTGRESQL!

--- postgres.js
+++ postgres.js
@@ -1,40 +1,16 @@
-const cool = require('cool-ascii-faces')
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+const {Client} = require('pg')
+const client = new Client ({
+  user: "vyrenyyniltmhq",
+  password: "9abd11ca0665ae17c7a91a80e41d2507f5e1746ced38b9483b446cd7624afa56",
+  host: "ec2-50-17-227-28.compute-1.amazonaws.com",
+  port: 5432,
+  database: "dbaskqc1scs4vk",
   ssl: true
-});
+})
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/times', (req, res) => res.send(showTimes()))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
-  .get('/cool', (req, res) => res.send(cool()))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
-  showTimes = () => {
-    let result = ''
-    const times = process.env.TIMES || 5
-    for (i = 0; i < times; i++) {
-      result += i + ' '
-    }
-    return result;
-  }
+client.connect()
+.then(() => console.log("Connected succesfully"))
+.then(() => client.query("SELECT * FROM files"))
+.then(results => console.table(results.rows))
+.catch(e => console.log(e))
+.finally(() => client.end())
