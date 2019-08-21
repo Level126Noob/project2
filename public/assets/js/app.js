64e0615f8781601eed02fdaf2727581ac6121baa
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-08-21 00:17:52 -0400

    merged, handlebars added.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,8 +1,34 @@
-const searchForm = document.querySelector('#searhForm')
-const grabSearch = () => {
-  console.log(searchForm.value)
-}
-// function to grab the text in the search bar
-document.getElementById('searchButton').addEventListener('click', grabSearch)
+$(".deletebutton").on("click", function (event) {
+  let id = $(this).data("id");
 
-// take that text and run it through postgres
+  $.ajax("/api/files/" + id, {
+      type: "DELETE"
+  }).then(
+      () => {
+          console.log("deleted id", id);
+          location.reload();
+      }
+  );
+});
+
+const searchInput = document.querySelector('#searchInput')
+const grabSearch = console.log(searchInput.value)
+
+//this needs a route written for it, I'm to tired. Going to bed
+//============================================================================
+$('#searchButton').click(function (e) {
+  e.preventDefault();
+  var search = $('#searchInput').val().trim();
+  console.log(search);
+
+  $.ajax("/" + search, {
+      type: "GET"
+  }).then(
+      () => {
+          console.log("search completed for " + search);
+          location.reload();
+      }
+  )
+
+});
+//=============================================================================
--- server.js
+++ server.js
@@ -1,52 +1,63 @@
-const express = require('express')
+const express = require("express");
 
-
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 8000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.static("public"));
 
-app.use(express.static('public'))
+const exphbs = require("express-handlebars");
 
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}))
 
-var exphbs = require('express-handlebars')
+app.set("view engine", "handlebars");
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
-
-var mysql = require('mysql')
-
-
+var mysql = require("mysql");
 var connection = mysql.createConnection({
-  host: 'localhost',
+  host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "scrapbook"
 });
 
-
-connection.connect(function (err) {
+connection.connect(function(err) {
   if (err) {
-    console.error('error connecting: ' + err.stack)
-    return
+    console.error("error connecting: " + err.stack);
+    return;
   }
-
   console.log("connected as id " + connection.threadId);
 });
 
 app.get("/", (_, res) => {
   connection.query("SELECT * FROM files", function (err, data) {
     if (err) {
-      return res.status(500).send("it's broken guys");
+      return res.status(500).send("it's broken dude");
     }
-    res.render("index", {products: data});
+
+    res.render("index", {
+      files: data
+    });
   });
 });
 
-app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost:" + PORT);
+app.delete("/api/files/:id", (req, res) => {
+  connection.query("DELETE FROM files WHERE id = ?", [req.params.id], function (err, result) {
+    if (err) {
+      return res.status(500).send("it's broken brohiem");
+    } else if (!result.affectedRows) {
+      return res.status(404).send("404 dude");
+    }
+    res.status(200).end();
+  });
 });
 
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
