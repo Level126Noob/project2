b0253ab61eb67a023c9e03efff70ad4322ea4ff4
Level126Noob <38739330+Level126Noob@users.noreply.github.com>
2019-09-04 11:41:46 -0400

    added jaws for heroku

--- server.js
+++ server.js
@@ -21,13 +21,16 @@ app.engine("handlebars", exphbs({
 app.set("view engine", "handlebars");
 
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "scrapbook"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'scrapbook'
+  })
+}
 
 connection.connect(function (err) {
   if (err) {
